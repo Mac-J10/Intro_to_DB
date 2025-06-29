7b3f1b7c7a6cdbd46dc3ab296a25cba8d2ffc558
Jackson <jacksonjay916@gmail.com>
2025-06-29 22:24:41 +0300

    REVISED DBMS

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -31,7 +31,7 @@ CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- create order_details table
@@ -40,6 +40,6 @@ CREATE TABLE Order_Details (
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
